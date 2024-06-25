b5fb8be0f1d4d75a5dee71c148a279800458603e
Chibueze Osuoji <qelvinz@gmail.com>
2024-06-25 11:52:54 +0100

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -13,7 +13,7 @@ const config = {
   authRequired: false,
   auth0Logout: true,
   secret: process.env.SECRET,
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://appointment-management-system.onrender.com/',
   clientID: process.env.CLIENT_ID,
   issuerBaseURL: 'https://dev-53cut7r7puvotgqa.us.auth0.com'
 };
